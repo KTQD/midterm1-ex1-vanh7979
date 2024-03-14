803b367ee69825d760c8c8e9b52e1f9f308b71e0
vanh7979 <137498913+vanh7979@users.noreply.github.com>
2024-03-14 07:49:30 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("em cua ngay hom qua", "son tung", "1234", 100000, 12);
+        Book book = new Book("harry poter", "phep thuat", "1234", 200000, "someone", 3000, "magic");
 
-        MP3 mp3;
         mp3.showInfo();
-
-        Book book;
         book.showInfo();
-
     }
 }
