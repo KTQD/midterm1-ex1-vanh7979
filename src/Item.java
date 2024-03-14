84d098dbade1a182c8155d905c2e7f7f516e136b
vanh7979 <137498913+vanh7979@users.noreply.github.com>
2024-03-14 07:53:57 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,26 @@
-public class Item {
+public abstract class Item {
+    public String name;
+    private String description;
+    protected String ID;
+    float price;
+
+
+
+    public Item(String name, String description, String ID, float price)
+    {
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+    public abstract void showInfo();
+
 }
